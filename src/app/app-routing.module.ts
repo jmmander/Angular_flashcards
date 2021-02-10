3a932d5bc98a90f24f6b4c9794ba499fc19068de
Jacqueline <maaaander@gmail.com>
2021-02-10 10:18:56 -0500

    added nav and home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FlashcardListComponent } from './components/flashcard-list/flashcard-list.component';
-import { FlashcardComponent } from './components/flashcard/flashcard.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { SetComponent } from './components/set/set.component'
 
 
 const routes: Routes = [{path: 'flashcards', component: FlashcardListComponent},
-                        {path: 'flashcard/:id', component: FlashcardComponent}
+                        {path: 'sets', component: SetComponent},
+                        {path: 'home', component: HomepageComponent}
                         ]
 
 @NgModule({
